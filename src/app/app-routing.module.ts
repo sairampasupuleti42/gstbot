08b5d7cb97e3c07022aa55ccd7a517099236e368
Sairam Pasupuleti <sairampasupuleti.42@gmail.com>
2020-10-26 01:47:44 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CalculatorComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
